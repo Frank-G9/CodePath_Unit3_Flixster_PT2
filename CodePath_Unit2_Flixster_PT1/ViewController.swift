c534d52d3534d550a516f198f256683f04d200a6
Frank Gomez <frank.gomez0923@gmail.com>
2023-09-11 09:59:35 -0400

    Initial Commit

---
+++ CodePath_Unit2_Flixster_PT1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodePath_Unit2_Flixster_PT1
+//
+//  Created by Frank Gomez on 9/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
